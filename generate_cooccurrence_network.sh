901eed92abfdcddb8be051dcb2ec176a0f7bcd64
Tony Lam <tonylam@Tonys-MacBook-Pro.local>
2022-01-22 20:23:29 -0800

    fixed numbering

--- generate_cooccurrence_network.sh
+++ generate_cooccurrence_network.sh
@@ -11,21 +11,21 @@ while read -r $ERR <&3; do
     ${LIB}/download_sra_quantify_bracken.fixed.sh ${ERR}
 done
 
-# 3) get id to taxa annotations
+# 2) get id to taxa annotations
 Rscript lib/get_taxa_annotations.R
 
-# 4) merge bracken outputs into phyloseq object
+# 3) merge bracken outputs into phyloseq object
 kraken-biom ../bracken_quantification/*/*s.breport -o sung_bracken.biom --fmt hdf5
 
-# 5) filter bracken results 
+# 4) filter bracken results 
 Rscript lib/get_bracken_by_disease.R -i 0_mk_phyloseq/sung_bracken.biom -m metadata.filtered.csv -o 3_abundance_matrix/ --ignore 2_samples2remove/ignore_these_sra.txt
 
-# 6) calculate spiec-easi - species wise
+# 5) calculate spiec-easi - species wise
 for i in "${arr[@]}";do
     Rscript ${WD}/lib/run_spiec_easi.mb.R ${WD}/3_abundance_matrix/${i}_bracken_filtered.s.csv ${WD}/4_spiec_easi_out/${i}_spiec.s.RData ${WD}/4_spiec_easi_out/${i}_spiec_mb.s.adj
 done
 
-# 7) make GML network files
+# 6) make GML network files
 for i in "${arr[@]}";do
     # build spiec-easi - species
     echo "${i}: se - s"
